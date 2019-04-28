7c1cc0e0bf7d7dd8f4eca9dbeed23f4d6ecacb4c
davidjumble <hellodavidparkinson@gmail.com>
2019-04-28 22:51:05 +0100

    finished

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -2,15 +2,7 @@ import React, { Component } from 'react';
 
 class Portfolio extends Component {
 
-    state = {
-        images: {
-            eye: 'maleTears'
-        },
-        titles: {
-            about: 'Aboutfsdfdsfdsfdsfdsfsdafsf',
-            eye: "Male Tears"
-        }
-    }
+
 
     render() {
 
@@ -19,12 +11,90 @@ class Portfolio extends Component {
         return (
 
             <section className="portfolio-box">
-                <div style={{ height: '20px' }}></div>
-                <hr className="blueline"></hr>
-                <div className="project">Project</div>
-                <hr className="blueline"></hr>
-                <div className='project-name'>{this.state.titles[openWork]}</div>
-                <img className={`${openWork}-image`} src={require(`../images/projectImages/${this.state.images.eye}.jpg`)} ></img>
+                <div className='project-info' >
+                    I wouldn’t say it was extremely comforting when, seconds before my first attempt at cold-water swimming at my local pond, as I hovered near the steps, my skin chill against the air, the jovial lifeguard started to tell me about sudden-immersion syndrome. “People can die, because they go into shock,” she explained, eyes focused on something in the distant trees. “But you’ll be fine. Just don’t panic.”
+
+        In the summer, I jump into the pond with the enthusiasm of a labrador – ears flying, paws spread skywards. (This was especially the case during last summer’s heatwave, two months of feeling conflicted – the utter joy of that slightly grassy smell of warm forearms, tempered by the fact that the planet is, well, screwed.) Summer swimming is all about the refreshing feel of hair slicked back from the forehead after hours of enduring a sticky, matted mess. Or the tessellating, shimmering patterns of light at the bottom of a hotel pool.
+
+
+        Cold-water swimming is like being plugged into the mains; there is the sensation of a sedentary life spent looking at screens cracking wide open. It’s a body slam-dunking in every department: heart, lungs, brain. The first time, I thought of Jack and Rose in Titanic, as she considers jumping overboard and he tries to talk her out of it: the water will “hit you like a thousand knives stabbing you all over your body”. (Incidentally, has there been a more handsome man than Leonardo DiCaprio in Titanic? This is a rhetorical question.)
+
+        Jack isn’t wrong, but knives have their uses. Knives can cut binds and set people free. At times I feel so bad that there is a restless ennui, a nihilistic velocity – every human atom bares its teeth; the shock of the cold water takes the edge off that inner violence. There has even been recent research that cold-water swimming can help with depression and anxiety, by improving the stress response.
+
+
+        My name is Hannah Jane Parkinson, and I'm a floor enthusiast
+         Read more
+        I’m no expert swimmer. I wear the same thing in all temperatures – a humble bikini. It’s from Topshop – or somewhere similar that might pay its taxes and isn’t owned by Philip Green. There are stronger swimmers who go more often than I do, kitted out in wetsuits and special waterproof thermal gloves and hats.
+
+        But even when it’s deep winter in the city and I look as if I took a wrong turn on the way to the Costa del Sol, I feel a sense of pride when I’ve met the challenge of 5C scrawled on the chalkboard, as satisfying as a maths equation solved. “Reinvigorating” is an overused word in an era of self-help books and journeys to faraway continents to find oneself, but cold-water swimming is truly that. And best of all? It’s free.
+
+        In search of inner calm and the ultimate natural high, growing numbers of Britons are braving freezing temperatures to swim in the great outdoors. From freshwater lakes and icy mountain pools to the thrashing waves of the sea, wild winter swimming – once the preserve of thrill-seekers and SAS training camps – is fast going mainstream.
+
+Hundreds of clubs have formed across the country, according to the Outdoor Swimming Society, which has seen membership soar to more than 70,000 over the past two years, compared with only a couple of hundred a decade ago.
+
+There are 200 mass swims this year – from Devon to the Scottish Highlands – but that is still unlikely to satisfy demand, according to Ella Foote, the group’s spokesperson. “They sell out like Glastonbury – we could fill these events twice over,” she said. And demand remains high despite hefty entry fees ranging from £50 to £150 a splash.
+
+
+Wild swimming: why I love jumping into icy cold lakes
+ Read more
+The trend for winter dipping has taken off in the past year, Foote believes, because swimmers do not want to go to indoor pools, where there are “so many rules” and you “don’t get the magic of the outdoors”. “Plus, cold water has so many health benefits – it improves your mood, increases blood circulation, it helps with muscular pain,” she said.
+
+“It feels bloody cold getting in but you become addicted to the effects of it – you feel great and buzzy all day,” said Mark Harper, who braves a chilly dip in the sea every morning with the Brighton swimming club.
+
+His club has become so popular, he said, that they have closed it to new members and a second one has opened down the road. Further along the south coast, near Poole, Dorset, 59-year-old Karen Biles started winter swimming five years ago and has never looked back.
+
+ Ella Foote takes to the water.
+ Ella Foote takes to the water. Photograph: @ellachloeswims
+“Ninety-nine per cent of my friends think I’ve completely lost the plot. But I don’t get migraines any more, I don’t get colds. I just feel much better all the time,” she said. And it’s not just the physical benefits: “You’re so busy concentrating on not getting too cold that anything that might have been troubling you is forgotten.”
+
+Suzanna Cruickshank, who runs a business that takes people on swimming tours in western Cumbria, agrees. “You’re not in [the water] for as long in the winter, but the experience is magnified,” she said. This is the first year Cruickshank has taken clients on winter swims, primarily because it is something they have been asking for. Last week she took a group of women for an ice swim in a mountain lake.
+
+A couple of days before the swim, she climbed up in the snow and cut a hole in the ice, to create a makeshift natural ice-bath that was big enough to swim in.
+
+“First they were all saying, ‘Why am I doing this?’, screaming and inching in bit by bit, catching their breath,” she said. “Then all of a sudden it just dissipates and you see this delight on their faces.”
+
+
+I wouldn’t say it was extremely comforting when, seconds before my first attempt at cold-water swimming at my local pond, as I hovered near the steps, my skin chill against the air, the jovial lifeguard started to tell me about sudden-immersion syndrome. “People can die, because they go into shock,” she explained, eyes focused on something in the distant trees. “But you’ll be fine. Just don’t panic.”
+
+        In the summer, I jump into the pond with the enthusiasm of a labrador – ears flying, paws spread skywards. (This was especially the case during last summer’s heatwave, two months of feeling conflicted – the utter joy of that slightly grassy smell of warm forearms, tempered by the fact that the planet is, well, screwed.) Summer swimming is all about the refreshing feel of hair slicked back from the forehead after hours of enduring a sticky, matted mess. Or the tessellating, shimmering patterns of light at the bottom of a hotel pool.
+
+
+        Cold-water swimming is like being plugged into the mains; there is the sensation of a sedentary life spent looking at screens cracking wide open. It’s a body slam-dunking in every department: heart, lungs, brain. The first time, I thought of Jack and Rose in Titanic, as she considers jumping overboard and he tries to talk her out of it: the water will “hit you like a thousand knives stabbing you all over your body”. (Incidentally, has there been a more handsome man than Leonardo DiCaprio in Titanic? This is a rhetorical question.)
+
+        Jack isn’t wrong, but knives have their uses. Knives can cut binds and set people free. At times I feel so bad that there is a restless ennui, a nihilistic velocity – every human atom bares its teeth; the shock of the cold water takes the edge off that inner violence. There has even been recent research that cold-water swimming can help with depression and anxiety, by improving the stress response.
+
+
+        My name is Hannah Jane Parkinson, and I'm a floor enthusiast
+         Read more
+        I’m no expert swimmer. I wear the same thing in all temperatures – a humble bikini. It’s from Topshop – or somewhere similar that might pay its taxes and isn’t owned by Philip Green. There are stronger swimmers who go more often than I do, kitted out in wetsuits and special waterproof thermal gloves and hats.
+
+        But even when it’s deep winter in the city and I look as if I took a wrong turn on the way to the Costa del Sol, I feel a sense of pride when I’ve met the challenge of 5C scrawled on the chalkboard, as satisfying as a maths equation solved. “Reinvigorating” is an overused word in an era of self-help books and journeys to faraway continents to find oneself, but cold-water swimming is truly that. And best of all? It’s free.
+
+        In search of inner calm and the ultimate natural high, growing numbers of Britons are braving freezing temperatures to swim in the great outdoors. From freshwater lakes and icy mountain pools to the thrashing waves of the sea, wild winter swimming – once the preserve of thrill-seekers and SAS training camps – is fast going mainstream.
+
+Hundreds of clubs have formed across the country, according to the Outdoor Swimming Society, which has seen membership soar to more than 70,000 over the past two years, compared with only a couple of hundred a decade ago.
+
+There are 200 mass swims this year – from Devon to the Scottish Highlands – but that is still unlikely to satisfy demand, according to Ella Foote, the group’s spokesperson. “They sell out like Glastonbury – we could fill these events twice over,” she said. And demand remains high despite hefty entry fees ranging from £50 to £150 a splash.
+
+
+Wild swimming: why I love jumping into icy cold lakes
+ Read more
+The trend for winter dipping has taken off in the past year, Foote believes, because swimmers do not want to go to indoor pools, where there are “so many rules” and you “don’t get the magic of the outdoors”. “Plus, cold water has so many health benefits – it improves your mood, increases blood circulation, it helps with muscular pain,” she said.
+
+“It feels bloody cold getting in but you become addicted to the effects of it – you feel great and buzzy all day,” said Mark Harper, who braves a chilly dip in the sea every morning with the Brighton swimming club.
+
+His club has become so popular, he said, that they have closed it to new members and a second one has opened down the road. Further along the south coast, near Poole, Dorset, 59-year-old Karen Biles started winter swimming five years ago and has never looked back.
+
+ Ella Foote takes to the water.
+ Ella Foote takes to the water. Photograph: @ellachloeswims
+“Ninety-nine per cent of my friends think I’ve completely lost the plot. But I don’t get migraines any more, I don’t get colds. I just feel much better all the time,” she said. And it’s not just the physical benefits: “You’re so busy concentrating on not getting too cold that anything that might have been troubling you is forgotten.”
+
+Suzanna Cruickshank, who runs a business that takes people on swimming tours in western Cumbria, agrees. “You’re not in [the water] for as long in the winter, but the experience is magnified,” she said. This is the first year Cruickshank has taken clients on winter swims, primarily because it is something they have been asking for. Last week she took a group of women for an ice swim in a mountain lake.
+
+A couple of days before the swim, she climbed up in the snow and cut a hole in the ice, to create a makeshift natural ice-bath that was big enough to swim in.
+
+“First they were all saying, ‘Why am I doing this?’, screaming and inching in bit by bit, catching their breath,” she said. “Then all of a sudden it just dissipates and you see this delight on their faces.”
+            </div>
 
             </section >
         );
--- src/pages/pictograms.js
+++ src/pages/pictograms.js
@@ -10,21 +10,26 @@ import Books from "./singlePictos/Books.js"
 import Gooey from "./singlePictos/Gooey.js"
 import Planets from "./singlePictos/Planets.js"
 import Portfolio from "./Portfolio"
+
+import ScrollToTop from "react-scroll-up"
 class Pictograms extends Component {
 
   state = {
-    about: '',
-    eye: '',
-    choirs: '',
-    english: '',
-    jam: '',
+    pictos: {
+      about: '',
+      eye: '',
+      choirs: '',
+      english: '',
+      jam: '',
+      ears: '',
+      free: '',
+      books: '',
+      gooey: '',
+      planets: '',
+      openPortfolio: '',
+    },
 
-    ears: '',
-    free: '',
-    books: '',
-    gooey: '',
-    planets: '',
-    openPortfolio: '',
+    pictoArray: ['about', 'eye', 'choirs', 'english', 'jam', 'ears', 'free', 'books', 'gooey', 'planetss']
 
   }
 
@@ -53,75 +58,128 @@ class Pictograms extends Component {
   }
 
   change(picto) {
-    this.state[picto] === '' ?
-      this.setState({ [picto]: 'changed', openPortfolio: picto }) :
-      this.setState({ [picto]: '' })
+    // this.state[picto] === '' ?
+    //   this.setState({ [picto]: 'changed', openPortfolio: picto }) :
+    //   this.setState({ [picto]: '' })
+
+    console.log(this.state);
+    let newPictos = {
+      openPortfolio: picto
+    };
+    this.state.pictoArray.forEach(el => {
+      el === picto ? (newPictos[el] = "changed") : (newPictos[el] = "");
+    });
+    this.setState({ pictos: newPictos });
   };
 
 
 
   render() {
 
+    const scrollStyle = {
+      position: 'fixed',
+      top: '3px',
+      left: '45%',
+      right: '0%',
+      cursor: 'pointer',
+      transitionDuration: '0.2s',
+      transitionTimingFunction: 'linear',
+      transitionDelay: '0s',
+      height: '100px',
+      width: '300px'
+    }
+
+
     return (
+
+
+
+
       <div>
-        <div className='burger' id='burger'></div>
-        <div className='burger-bar'></div>
-        <div className='menu' id="menu">
+        <div className='burger yellow' id='burger'>
+          <ScrollToTop showUnder={0} style={scrollStyle}>
+            <img className='burgerimage' src={require('./burgermenu.png')} />
+            <span></span><span></span>
+          </ScrollToTop>
+        </div>
+        <div className='burger-bar yellow'></div>
+        <div className='menu yellow' id="menu">
           <div className='bbc'  >
             <hr className="line-1"></hr>
-            <header className='bbc-type-1' >INFORM ,  EDUCATE AND ENTERTAIN</header>
-            <header className='bbc-type-2'>ANNNND. HIRE DAVID PARKINSON</header>
+            <header className='bbc-type-1' >A NEAT SHRINKING BURGER MENU</header>
+            <header className='bbc-type-2'>OF OBLONG PICTOGRAM CHOICES</header>
             <hr className="line-2"></hr>
           </div>
 
           < section className='all-pictos' id="picto-box" >
 
 
-            <div className="about-box height red" onClick={() => { this.change('about') }} >
-              <About about={this.state.about} />
+            <div className="red about-box height "
+
+              style={{ borderColor: this.state.pictos.openPortfolio != 'about' ? 'black' : 'orange' }}
+
+
+              onClick={() => { this.change('about') }} >
+              <About about={this.state.pictos.about} />
             </div>
 
-            <div className="eye-box height green" onClick={() => { this.change('eye') }} >
-              <Eye eye={this.state.eye} />
+            <div className="red eye-box height"
+              style={{ borderColor: this.state.pictos.openPortfolio != 'eye' ? 'black' : 'orange' }}
+              onClick={() => { this.change('eye') }} >
+              <Eye eye={this.state.pictos.eye} />
             </div>
 
-            <div className="choirs-box height red" onClick={() => { this.change('choirs') }} >
+            <div className="choirs-box height red" onClick={() => { this.change('choirs') }}
+              style={{ borderColor: this.state.pictos.openPortfolio != 'choirs' ? 'black' : 'orange' }}
+            >
               <Choirs eye={this.state.eye} />
             </div>
 
-            <div className="english-box height red" onClick={() => { this.change('english') }} >
+            <div className="english-box height red" onClick={() => { this.change('english') }}
+
+              style={{ borderColor: this.state.pictos.openPortfolio != 'english' ? 'black' : 'orange' }}>
               <English english={this.state.english} />
             </div>
 
-            <div className="jam-box height red" onClick={() => { this.change('jam') }} >
+            <div className="jam-box height red" onClick={() => { this.change('jam') }}
+              style={{ borderColor: this.state.pictos.openPortfolio != 'jam' ? 'black' : 'orange' }} >
               <Jam jam={this.state.jam} />
             </div>
 
-            <div className="ears-box height red" onClick={() => { this.change('ears') }} >
+            <div className="ears-box height red" onClick={() => { this.change('ears') }}
+              style={{ borderColor: this.state.pictos.openPortfolio != 'ears' ? 'black' : 'orange' }}
+            >
               <Ears ears={this.state.ears} />
             </div>
 
-            <div className="free-box height red" onClick={() => { this.change('free') }} >
+            <div className="free-box height red" onClick={() => { this.change('free') }}
+              style={{ borderColor: this.state.pictos.openPortfolio != 'free' ? 'black' : 'orange' }}
+            >
               <Free free={this.state.free} />
             </div>
 
-            <div className="books-box height red" onClick={() => { this.change('books') }} >
+            <div className="books-box height red" onClick={() => { this.change('books') }}
+
+              style={{ borderColor: this.state.pictos.openPortfolio != 'books' ? 'black' : 'orange' }}>
               <Books books={this.state.books} />
             </div>
 
-            <div className="gooey-box height red" onClick={() => { this.change('gooey') }} >
+            <div className="gooey-box height red" onClick={() => { this.change('gooey') }}
+
+              style={{ borderColor: this.state.pictos.openPortfolio != 'gooey' ? 'black' : 'orange' }}>
               <Gooey gooey={this.state.gooey} />
             </div>
 
-            <div className="planets-box height red" onClick={() => { this.change('planets') }} >
+            <div className="planets-box height red" onClick={() => { this.change('planets') }}
+              style={{ borderColor: this.state.pictos.openPortfolio != 'planets' ? 'black' : 'orange' }}>
               <Planets planets={this.state.planets} />
             </div>
           </section>
         </div>
 
 
-        <Portfolio openWork={this.state.openPortfolio} />
-      </div>
+        <Portfolio openWork={this.state.pictos.openPortfolio} />
+      </div >
     );
   }
 }
